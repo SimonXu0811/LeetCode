1796c2ce604abaf3f151addbff10f77cb6636f36
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-03-30 17:40:33 +0800

    Update Code.java

--- 189/Code.java
+++ 189/Code.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public void rotate(int[] nums, int k) {
         if(k==0){
             return;
@@ -11,3 +11,34 @@ class Solution {
         rotate(nums,k-1);
     }
 }
+
+/*
+时间复杂度:O(n*k)
+空间复杂度:O(1)
+*/
+
+
+
+public class Solution2 {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+}
+
+/*
+时间复杂度:O(n)
+空间复杂度:O(1)
+*/
