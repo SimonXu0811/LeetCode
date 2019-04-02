9e7e03d3aa4df8d36e96615641f2b5f67bfbf6ca
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-04-02 16:02:59 +0800

    Create Code.java

---
+++ 674/Code.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int ans = 0, anchor = 0;
+        for (int i = 0; i < nums.length; ++i) {//从头向后遍历数组
+            if (i > 0 && nums[i-1] >= nums[i]) anchor = i;//记录递减数的索引
+            ans = Math.max(ans, i - anchor + 1);//取最大值
+        }
+        return ans;
+    }
+}
+
+/*
+时间复杂度:O(n)
+空间复杂度:O(1)
+*/
