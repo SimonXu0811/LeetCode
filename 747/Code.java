89d1fd0933e281cd6c24a6eb07fe35d3b3c8c210
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-04-04 09:49:09 +0800

    Update Code.java

--- 747/Code.java
+++ 747/Code.java
@@ -1,11 +1,11 @@
 class Solution {
     public int dominantIndex(int[] nums) {
         int maxIndex = 0;
-        for (int i = 0; i < nums.length; ++i) {//找最大值及索引
+        for (int i = 0; i < nums.length; i++) {//找最大值及索引
             if (nums[i] > nums[maxIndex])
                 maxIndex = i;
         }
-        for (int i = 0; i < nums.length; ++i) {//比较
+        for (int i = 0; i < nums.length; i++) {//比较
             if (maxIndex != i && nums[maxIndex] < 2 * nums[i])
                 return -1;
         }
