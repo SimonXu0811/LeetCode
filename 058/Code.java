238ab0cb67311e9f34ccbd38387a5bf9d920023a
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-04-04 09:57:10 +0800

    Update Code.java

--- 058/Code.java
+++ 058/Code.java
@@ -2,22 +2,21 @@ class Solution {
     public int lengthOfLastWord(String s) {
     int len = s.length()-1;
     int ans = 0;
-    boolean consume = true;
+    boolean consume = true;//判断是否是末尾的
     while (len >= 0) {
-        if (s.charAt(len) != ' ') {
+        if (s.charAt(len) != ' ') {//不是空格就向前，长度加一
             len--;
             ans++;
             if (consume)
-                consume = false;
+                consume = false;//这时后面遇到的空格就不是末尾空格了
         }
-        else if (consume)
+        else if (consume)//如果是末尾空格的话，就继续向前
             len--;
         else
             break;
     }
     
     return ans;
-    
 }
 }
 
