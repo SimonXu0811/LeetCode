bf19073183ca4e79f78ae7db1bcd76340b7406e3
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-04-09 15:41:43 +0800

    Create Code.java

---
+++ 104/Code.java
@@ -0,0 +1,11 @@
+public int maxDepth(TreeNode root) {
+    if(root==null){//空树
+        return 0;
+    }
+    return 1+Math.max(maxDepth(root.left),maxDepth(root.right));//取出哪个叶子结点还存在的 然后继续向下找
+}
+
+/*
+时间复杂度:O(n)
+空间复杂度:O(1)
+*/
