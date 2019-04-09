30bd2efe14f841ab8687ea2324b84f65beb0171f
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-04-09 16:51:48 +0800

    Create Code.java

---
+++ 404/Code.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null) return 0;
+        int sum = 0;
+        if(root.left != null && root.left.left ==null && root.left.right ==null){//判断是不是叶子结点
+            sum += root.left.val;//如果符合就加起来
+        }else{
+            sum += sumOfLeftLeaves(root.left);//继续遍历下面的结点
+        }
+        sum += sumOfLeftLeaves(root.right);//找右子树
+        return sum;
+    }
+}
+
+/*
+时间复杂度:O(n)
+空间复杂度:O(n)
+*/
