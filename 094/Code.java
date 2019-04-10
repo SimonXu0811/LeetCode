ce6a6f0285ac4c6c0540c3f56a27c5e7db122b36
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-04-10 17:19:21 +0800

    Update Code.java

--- 094/Code.java
+++ 094/Code.java
@@ -24,3 +24,36 @@ class Solution {
 时间复杂度:O(n)
 空间复杂度:O(n)
 */
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List < Integer > inorderTraversal(TreeNode root) {
+        List < Integer > res = new ArrayList < > ();
+        Stack < TreeNode > stack = new Stack < > ();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {//从左一直遍历到最左侧的节点
+                stack.push(curr);//把左侧节点放进栈中
+                curr = curr.left;
+            }
+            curr = stack.pop();//然后pop掉最下面的
+            res.add(curr.val);//加进栈中
+            curr = curr.right;//开始找最右侧
+        }
+        return res;
+    }
+}
+
+/*
+时间复杂度:O(n)
+空间复杂度:O(n)
+*/
