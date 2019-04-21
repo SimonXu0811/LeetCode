ed76770f027937a5c2da09755331d1ad104e091b
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-04-21 21:34:40 +0800

    Create Code.java

---
+++ 744/Code.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int low = 0, high = letters.length;
+        while (low < high) {//二分查找
+            int mid = low + (high - low) / 2;
+            if (letters[mid] <= target) low = mid + 1;//如果不比目标大，那么就后一半
+            else high = mid;//否则就前一半
+        }
+        return letters[low % letters.length];
+    }
+}
+
+/*
+时间复杂度:O(logn)
+空间复杂度:O(1)
+*/
