3deb5115c0276c952103fe1a6b5791de3a7d4a13
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-04-21 21:21:30 +0800

    Create Code.java

---
+++ 475/Code.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+		Arrays.sort(heaters);
+		
+		int result = 0;
+		for(int house:houses) {
+			int index = Arrays.binarySearch(heaters, house);
+			if(index<0) { //index<0,则说明在headers中没有该house,返回 (-(插入点) - 1),第一个大于此键的元素索引  
+				index = ~index;
+				int dist1 = index - 1 >= 0 ? house - heaters[index - 1] : Integer.MAX_VALUE;
+	                        int dist2 = index < heaters.length ? heaters[index] - house : Integer.MAX_VALUE;
+ 
+	                        result = Math.max(result, Math.min(dist1, dist2));
+			}
+			//index>=0,则说明在headers中找到了该house, 则说明取暖器位置和house位置重合，该house的最小半径为0,result不变
+		}
+		return result;
+    }
+}
+
+/*
+时间复杂度:O(nlogn)
+空间复杂度:O(1)
+*/
