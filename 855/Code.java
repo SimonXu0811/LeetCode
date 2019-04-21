5a0260834ad80e547a5a4c8781cc903b28804dc7
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-04-21 20:56:55 +0800

    Create Code.java

---
+++ 855/Code.java
@@ -0,0 +1,47 @@
+class ExamRoom {
+    int N;
+    TreeSet<Integer> students;
+
+    public ExamRoom(int N) {
+        this.N = N;
+        students = new TreeSet();
+    }
+
+    public int seat() {
+        int student = 0;//这个用来记录长度
+        if (students.size() > 0) {
+            int dist = students.first();
+            Integer prev = null;
+            for (Integer s: students) {//遍历整个set，如果有值，那么记录位置，然后取中间，每次加上中间去前面的距离
+                if (prev != null) {
+                    int d = (s - prev) / 2;
+                    if (d > dist) {
+                        dist = d;
+                        student = prev + d;
+                    }
+                }
+                prev = s;
+            }
+            if (N - 1 - students.last() > dist)//如果加上距离超过了最后的位置，那么就放最后的位置
+                student = N - 1;
+        }
+        students.add(student);
+        return student;
+    }
+
+    public void leave(int p) {
+        students.remove(p);
+    }
+}
+
+/**
+ * Your ExamRoom object will be instantiated and called as such:
+ * ExamRoom obj = new ExamRoom(N);
+ * int param_1 = obj.seat();
+ * obj.leave(p);
+ */
+ 
+ /*
+ 时间复杂度:O(P)
+ 空间复杂度:O(P)
+ */
