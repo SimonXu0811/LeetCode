505a98cef0a532cddf9eba7214154c347460a71a
Simon <48972207+SimonXu0811@users.noreply.github.com>
2019-05-23 11:40:18 +0800

    Update Code.java

--- 090/Code.java
+++ 090/Code.java
@@ -1,19 +1,18 @@
-class Solution {
-    List<List<Integer>> res = new LinkedList<>();
+public class Solution {
+    List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);//先整理 找出相同元素
-        BackTrace(nums,0,new LinkedList<>());
+        Arrays.sort(nums);
+        dfs(nums, 0, new ArrayList<Integer>());
         return res;
     }
     
-    private void BackTrace(int[] nums, int n, List<Integer> list){
-        if(res.contains(list)) return;//如果有了就不加
-        res.add(new LinkedList(list));
-        
-        for(int i = n; i < nums.length; i++){//回溯将所有元素都加进去
-            list.add(nums[i]);
-            BackTrace(nums, i + 1, list);//加到底
-            list.remove(list.size() - 1);//回溯
+    public void dfs(int[] nums,int index,List<Integer> path){
+        res.add(path);
+        for(int i=index; i<nums.length; i++){
+            if(i > index&&nums[i]==nums[i - 1]) continue;//如果遇到重复的就向后移动，创建新的记录，旧的依旧保存
+            List<Integer> nPath= new ArrayList<>(path);
+            nPath.add(nums[i]);
+            dfs(nums, i + 1, nPath);
         }
     }
 }
